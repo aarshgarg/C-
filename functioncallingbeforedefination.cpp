40f77a06d439ab647a7465e83f0208687208cafe
aarshgarg <aarshgarg0@gmail.com>
2021-12-21 01:32:53 +0530

    function call before defination

---
+++ functioncallingbeforedefination.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void nemo(int ,int );
+
+int main()
+{
+   // int sum;
+    nemo(54,45);
+    
+    return 0;
+}
+
+ void nemo(int a,int b)
+{
+    int sum;
+    sum=a+b; 
+       cout<<"sum of the number"<<sum;
+}
