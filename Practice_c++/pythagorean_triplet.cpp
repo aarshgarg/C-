2930948d7cd360ecb1c29f5a76dced43d0916a48
aarshgarg <aarshgarg0@gmail.com>
2022-04-05 23:56:34 +0530

    pythagoren triplet

---
+++ Practice_c++/pythagorean_triplet.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+bool check(int x,int y,int z)
+{
+    int a=max(x,max(y,z));
+    int b,c;
+    if(a==x)
+    {
+        b=y;
+        c=z;
+    }
+    else if(a==y)
+    {
+        b=z;
+        c=x;
+    }
+    else{
+        b=x;
+        c=y;
+    }
+
+    if(a*a==b*b+c*c)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+int main()
+{
+    int x,y,z;
+    cout<<"Enter the number=";
+    cin>>x>>y>>z;
+    if (check(x,y,z))
+    {
+        cout<<"Pythagoren triplet";
+    }
+    else
+    {
+        cout<<"Not pythagoren triplet";
+    }
+
+}
