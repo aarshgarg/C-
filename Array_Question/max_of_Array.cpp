39e1ffdfacdcfa4035ed767ebd5fea25fce2178e
aarshgarg <aarshgarg0@gmail.com>
2022-04-11 01:40:08 +0530

    max of array

---
+++ Array_Question/max_of_Array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //  we can take the -ve value due to the if we update the value of max then it can
+    //   change easily otherwise if we can intializie the value 0 and 
+    //   you can print the first element as -1 it cannot update  
+    int mx=-9;
+    int n;
+    cout<<"Enter the number=";
+    cin>>n;
+    int arr[n];
+    for( int i=0;i<n;i++)
+    {
+        cout<<"enter the elements in array=";
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+{
+    mx= max(mx,arr[i]);
+    cout<<mx<<endl;
+}
+return 0;
+}
