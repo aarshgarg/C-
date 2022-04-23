200b16c2e7a6b88be045c1c04b642482a16fd36c
aarshgarg <aarshgarg0@gmail.com>
2022-04-23 23:29:49 +0530

    Sieve of Eratosthenes

---
+++ Practice_c++/Sieve_of_Eratosthenes.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void primeSieve(int n)
+{
+    int prime[100]={0};
+
+    for(int i=2;i<=n;i++)
+    {
+        // unmarked
+        if(prime[i]==0)
+        {
+            for(int j=i*i;j<=n;j+=i)
+            {
+                prime[j]=1;
+            }
+        }
+    }
+    for(int i=2;i<=n;i++)
+    {
+        if(prime[i]==0)
+        {
+            cout<<i<<" ";
+        }
+    }
+    cout<<endl;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the number to print the prime number=";
+    cin>>n;
+
+    primeSieve(n);
+
+    return 0;
+
+
+}
